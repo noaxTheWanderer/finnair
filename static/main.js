e58d4b8b72b65cae1ae76686c54a384cbea3e1da
Mika <mika.hamalainen@mail.com>
2017-11-26 12:23:29 +0200

    changes

--- static/main.js
+++ static/main.js
@@ -23,7 +23,7 @@ function update_listing(data){
 	var html = "";
 	for (var i = 0; i < data.length; i++) {
 		var item = data[i];
-		html = html +"<div class='col-lg-4 col-sm-6 portfolio-item'><div class='card h-100'><a href='#''><img class='card-img-top' src='"+ item["image"] +"'></a>";
+		html = html +"<div class='col-lg-4 col-sm-6 portfolio-item'><div class='card h-100'><a href='#'><img class='card-img-top' src='"+ item["image"] +"'></a>";
 		html = html + '<div class="card-body"><h4 class="card-title"><a href="#">'+item["name"]+'</a></h4><h5>'+ item["tagline"] +'</h5>';
 		html = html + "<p>" + item["flight-time"]+ "</p>";
 		html = html + '<button type="button" onclick="showMore(event)" class="btn btn-secondary">...</button><button type="button" class="btn btn-success">From '+ item["price"]+'</button>';
